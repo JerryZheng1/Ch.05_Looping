f4e5aa4c592673f0b7138efc53ffe4d9583c4b4c
zhengjer <jerryzheng060923@gmail.com>
2024-01-22 10:02:07 -0600

    FINAL COMMIT

--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -6,21 +6,22 @@
      print("This program takes three integers and returns the sum.")
      total = 0
      for i in range(3):
-         x = input("Enter a number: ")
-         total+=i
-     print("The total is:", x)
+         x = int(input("Enter a number: "))
+         total+=x
+     print("The total is:", total)
 
-#1
-#2
-#3
+#1 make int(input("Enter a number:")
+#2 total+=x
+#3 change "The total is ", total)
 
 
 '''
   2. Write a Python program that will use a FOR loop to print the even
-     numbers from 2 to 100, inclusive. (5pts)
+     numbers from 2 to 100, inclusive (includes 2 and 100). (5pts)
 '''
 
-
+for i in range(2,102,2):
+    print(i)
 
 
 
@@ -30,14 +31,22 @@
      a WHILE loop, don't use a FOR loop. (5pts)
 '''
 
-
+i=10
+while i>-1:
+    print(i)
+    i-=1
+print("Blast off!")
 
 
 
 
 '''
-  4. Write a program that prints a random integer from 1 to 10 (inclusive). (5pts)
+  4. Write a program that prints a random integer from 1 to 10 (inclusive) (includes 1, 10). (5pts)
 '''
+
+import random
+my_number=random.randint(1,10)
+print(my_number)
 
 
 
@@ -54,7 +63,24 @@
      if statements.
       
 '''
+total=0
+positive=0
+negative=0
+zero=0
+for i in range(7):
+    input_number=float(input("Enter a number"))
+    total+=input_number
+    if input_number>0:
+        positive+=1
+    elif input_number==0:
+        zero+=1
+    else:
+        negative+=1
 
+print("you sum is:",total)
+print("You entered a total of",positive,"positive numbers")
+print("You entered a total of",negative,"negative numbers")
+print("You entered a total of",zero,"zeros")
 
 
 
@@ -68,6 +94,21 @@
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
+import random
+heads=0
+tails=0
+for i in range(50):
+    number=random.randint(0,1)
+    if number==1:
+        print("heads")
+        heads+=1
+    else:
+        print("tails")
+        tails+=1
+print("")
+print("TOTAL:")
+print("You got",heads,"heads")
+print("You got",tails,"tails")
 
 
 
@@ -86,8 +127,63 @@ Each round, tell me what the computer chose, what I chose and also if I won, los
 When the user quits, print an end game message and their win/loss/tie record
 
 '''
+import random
+done = False
+user_input=0
+computer_input=0
+computer_score=0
+user_score=0
+tied_score=0
 
-
-
-
+print("ROSHAMBO PROGRAM V1.0")
+while not done:
+    print("")
+    user_input=int(input("Choose an option, 1 for rock, 2 for paper, 3 for scissors and 4 to quit."))
+    print("")
+    if user_input==4:
+        done = True
+    elif user_input==1 or user_input==2 or user_input==3:
+        if user_input==1:
+            print("You chose Rock")
+        elif user_input == 2:
+            print("You chose Paper")
+        else:
+            print("You chose Scissors")
+        computer_input=random.randint(1,3)
+        if computer_input==1:
+            print("Computer chooses Rock")
+        elif computer_input==2:
+            print("Computer chooses paper")
+        else:
+            print("Computer chooses scissors")
+        if computer_input==user_input:
+            print("It's a tie!")
+            tied_score+=1
+        elif user_input==1 and computer_input==2:
+            print("The Computer won! Paper > Rock")
+            computer_score+=1
+        elif user_input == 1 and computer_input == 3:
+            print("You won! Rock > Scissors")
+            user_score+=1
+        elif user_input == 2 and computer_input == 3:
+            print("The Computer won! Scissors > Paper")
+            computer_score += 1
+        elif user_input == 2 and computer_input == 1:
+            print("You won! Paper > Rock")
+            user_score+=1
+        elif user_input == 3 and computer_input == 1:
+            print("The Computer won! Rock > Scissors")
+            computer_score += 1
+        elif user_input == 3 and computer_input == 2:
+            print("You won! Scissors > Paper")
+            user_score += 1
+        else:
+            print("Hey that's not an option!")
+    else:
+        print("Hey that's not an option!")
+print("")
+print("YOUR FINAL SCORE")
+print("WINS:",user_score)
+print("LOSES:",computer_score)
+print("TIES:",tied_score)
 
--- CAMEL_GAME_PROJECT.py
+++ CAMEL_GAME_PROJECT.py
@@ -4,12 +4,245 @@ CAMEL GAME
 The pseudo-code for how to code this game is in Chapter 5 of the Python Jedi book
 
 '''
+import random
+
+#all the vairables set to 0 at the beginning of the game
+
+done=False
+miles_traveled_total=0
+fuel=200
+hunger=0
+zombie_distance=-20
+foodcans_left=10
+miles_driven_inturn=0
+tiredness=0
+cans_gas_left=100
+win=0
+gas_station_found=0
+foodcans_find=0
+x=0
+y=0
+a=0
+b=0
+
+#introduction
+
+print("\033[1;33;40mWhat difficulty would you like to play the game at?")
+print("")
+print("A.Easy\nB.Medium\nC.HARD")
+diff=(input("\033[1;33;40m"))
+
+if diff.lower()=="a":
+    x=5
+    y=10
+    a=2
+    b=2
+elif diff.lower()=="b":
+    x=10
+    y=15
+    a=3
+    b=3
+elif diff.lower()=="c":
+    x=20
+    y=25
+    a=4
+    b=4
+
+print("\033[1;31;40mGo! Go! Go! The zombie horde is right behind us.")
+print("Get into the truck, you have to outrun them.")
+print("Drive through the city to the military base before the zombie reaches you.")
+print("\033[1;31;40m")
+print("")
+
+
+
+while not done:
+    if foodcans_find==1:
+        print("\033[1;33;40mYou found 10 cans of food randomly on the streets")
+        foodcans_left+=10
+        foodcans_find=0
+
+
+    if gas_station_found==1:
+        print("")
+        print("\033[1;33;40mYou found a empty gas station along the way!")
+        print("You found 20 cans of food and refilled your truck")
+        print("")
+        fuel=200
+        foodcans_left+=20
+        gas_station_found=0
+    if miles_traveled_total>200: #winning message
+        win = 1
+        done = True
+        break
+
+    if (miles_traveled_total-zombie_distance)>0 and (miles_traveled_total-zombie_distance)<15:
+        print("\033[1;31;40mThe zombies are getting close!")
+        print("")
+
+    elif (miles_traveled_total-zombie_distance)<0:
+        print("The zombies caught up!")
+        print("")
+        done = True
+        break
+
+    if not done and hunger > 15 and hunger <= 20: #died of hunger
+        print("\033[1;31;40mYou are hungry!")
+        print("")
+    elif not done and hunger > 20:
+        print("\033[1;31;40mYou died of hunger!")
+        print("")
+        done = True
+        break
+
+    if not done and fuel<=25 and fuel>0: #died of no fuel in truck
+        print("\033[1;31;40mYou are getting low on gas.")
+        print("")
+
+    elif not done and done==True:
+        break
+
+    elif not done and fuel<=0:
+        print("\033[1;31;40mYour car ran out of gas!")
+        print("")
+        done = True
+        break
+
+    if not done and tiredness > 15 and tiredness <= 20: #died of hunger
+        print("\033[1;31;40mYou are tired!")
+        print("")
+    elif not done and tiredness > 20:
+        print("\033[1;31;40mYou are too tired and died!")
+        done = True
+        print("")
 
 
 
 
+    print("\033[1;32;40mChoose your options:")
+    print("A: Refuel the truck")
+    print("B: Move ahead at moderate speed")
+    print("C: Move ahead at full speed")
+    print("D: Stop to rest and eat")
+    print("E. Attempt to find food")
+    print("F: Status check")
+    print("Q: Quit")
+    print("")
+    user_input=input("What is your option?")
+
+    if user_input.upper()=="Q": #option quit
+        win=2
+        done=True
+
+
+    elif user_input.upper()=="F": #option status
+        print("")
+        print("\033[1;37;40mStatus:")
+        print("Miles driven:",miles_traveled_total)
+        print("Food cans left:",foodcans_left)
+        print("The Zombie Horde is",(miles_traveled_total-zombie_distance),"miles behind you")
+        print("Gas left:",fuel,"gallons")
+        print("Hunger Status:",hunger)
+        print("Tiredness:",tiredness)
+        print("Extra gas cans:",cans_gas_left,"gallons")
+        print("")
+
+
+    elif user_input.upper()=="D": #option rest, sets tiredness=0 and hunger = 0
+        print("\033[1;35;40m")
+        print("You stopped to eat and rest for an hour")
+        print("")
+        tiredness=0
+        if foodcans_left>0:
+            print("")
+            hunger = 0
+            foodcans_left-=3
+            zombie_distance += random.randint(5, 15)
+        else:
+            print("You have no food left :(")
+            zombie_distance += random.randint(5,15)
+
+
+    elif user_input.upper()=="C": #move ahead at full speed
+        print("\033[1;36;40m")
+        miles_driven_inturn=0
+        print("---------------------------------------------------------")
+        print("You drove ahead at full speed")
+        print("")
+        miles_driven_inturn+=random.randint(15,25)
+        hunger+=a
+        tiredness+=b
+        fuel-=(miles_driven_inturn)*(1.25)
+        miles_traveled_total+=miles_driven_inturn
+        print("You drive",miles_driven_inturn,"miles ahead")
+        print("")
+        print("In total, you have drove",miles_traveled_total,"miles")
+        print("---------------------------------------------------------")
+        gas_station_found= random.randint(0,20)
+        zombie_distance += random.randint(x, y)
+
+
+    elif user_input.upper()=="B": #move ahead at moderate speed
+        print("\033[1;36;40m")
+        miles_driven_inturn = 0
+        print("---------------------------------------------------------")
+        print("You drove ahead at moderate speed")
+        print("")
+        miles_driven_inturn += random.randint(15, 20)
+        hunger += 1
+        tiredness +=1
+        fuel -= (miles_driven_inturn)
+        miles_traveled_total += miles_driven_inturn
+        print("You drive", miles_driven_inturn, "miles ahead")
+        print("")
+        print("In total, you have drove", miles_traveled_total, "miles")
+        gas_station_found = random.randint(0, 20)
+        print("---------------------------------------------------------")
+        zombie_distance += random.randint(x, y)
+
+    elif user_input.upper()=="E": #tries to find food
+        print("\033[1;35;40m")
+        foodcans_find = random.randint(0, 3)
+        zombie_distance+=random.randint(x,y)
+        if foodcans_find!=1:
+            print("You found nothing")
 
 
 
 
+    elif user_input.upper()=="A": #refuels the truck, adds 50 cans to the tank
+        print("\033[1;35;40m")
 
+        if cans_gas_left>0:
+            print("You refilled your truck by 50 cans")
+            print("")
+            fuel+=50
+            cans_gas_left-=50
+        else:
+            print("You have no extra gas cans left :(")
+            zombie_distance +=random.randint(3,5)
+
+    elif miles_traveled_total>200: #winning message
+        win = 1
+        done = True
+        break
+
+
+
+
+if win==1: #win message, where it detects win==1 to give message of winning
+    print("\033[1;33;40mCongrats on getting to the military base")
+    print("You feel safe knowing the military will protect you from the zombies")
+    print("")
+    print("")
+    print("\033[1;34;40m5 Years later, with your help of killing zombies, the world has returned to normal")
+
+elif win==2:
+    print("\033[1;33;40mThanks for playing!")
+
+else:
+    print("\033[1;31;40mR.I.P")
+    print("")
+    print("")
+    print("5 years later, the zombies takes over the world. Humans are now extinct.")
+    print("\033[1;33;40mThanks for playing!")
